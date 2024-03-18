1dc5b6bb2bb2bf2e1479207fae99ca75abc7dbc8
LoamySand <laneeboyd@gmail.com>
2024-03-18 17:08:15 -0500

    front end login page

---
+++ routes/register.js
@@ -0,0 +1,10 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET register page. */
+router.get('/register', function (req, res) {
+
+});
+
+module.exports = router;
